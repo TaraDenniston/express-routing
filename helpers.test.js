7eb6b46f89204e776036c6c67f3d058b9b49701a
Tara Denniston <taradenniston@gmail.com>
2023-09-21 19:11:51 -0400

    add tests for convertToNums function

--- helpers.test.js
+++ helpers.test.js
@@ -1,6 +1,24 @@
 const { convertToNums, calculateMean } = require('./helpers');
 
 
+describe('convertToNums function', () => {
+  test('converts strings into integers', () => {
+    let numsArray = convertToNums(['1', '2', '3', '4']);
+    expect(numsArray).toEqual([1, 2, 3, 4]);
+  });
+
+  test('converts strings into negative numbers and decimals', () => {
+    let numsArray = convertToNums(['-1', '2.5', '0', '-4.39']);
+    expect(numsArray).toEqual([-1, 2.5, 0, -4.39]);
+  });
+
+  test('returns error when given an array containing NaN', () => {
+    let numsArray = convertToNums(['1', '2', '3', 'hello']);
+    expect(numsArray).toBeInstanceOf(Error);
+  });
+})
+
+
 describe('calculateMean function', () => {
   test('returns mean from integers', () => {
     let mean = calculateMean([2,4,6,8]);
